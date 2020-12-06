8cbcc53d371e7009da511279d299dcebbab0b9f2
marinamare <marinamaredesign@gmail.com>
2020-12-06 14:18:32 -0300

    atualização schema

--- src/models/empreendimentos.js
+++ src/models/empreendimentos.js
@@ -5,8 +5,7 @@ const empreendimentosSchema = new mongoose.Schema(
     id: { type: Number },
     nome: { type: String },
     referênciaVeganaEm: { type: String },
-    telefone: { type: Number },
-    site: { type: String },
+    contato: { type: String },
     empreedimentoFísico: { type: Boolean },
     cidadeEstado: { type: String },
     statusAtiva: { type: Boolean }
